c95b0ed86dae3caba35e175ec66575da68c074d2
Nicole Durrant-Eunson <ndurrant-eunson@rrc.ca>
2026-05-10 22:59:50 -0500

    Adjusted code for the way the employee list is rendered in app.js

--- app.js
+++ app.js
@@ -7,23 +7,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const main = document.getElementById("main");
     main.innerHTML = "";
 
+    // Gets the department and employee data from data.js
     departments.forEach((department) => {
         const section = document.createElement("section");
 
+        // Creates the heading for the departments
         const heading = document.createElement("h2");
         heading.textContent = department.name;
         section.appendChild(heading);
 
+        // Creates the list of employees for each department
         const ul = document.createElement("ul");
-
+        
+        // Iterates through the employees in each department and creates
+        // a list item for each employee, then appends it to the unordered list
         department.employees.forEach((employee) => {
             const li = document.createElement("li");
-            li.textContent = employee.lastName
-                ? `${employee.firstName} ${employee.lastName}`
-                : employee.firstName;
+            li.textContent = `${employee.firstName} ${employee.lastName}`;
             ul.appendChild(li);
         });
 
+        // Appends the list of employees to section and then appends section to main
         section.appendChild(ul);
         main.appendChild(section);
     });
