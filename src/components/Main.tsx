4691046b10b104e3ce83841e184794575eab9535
Nicole Durrant-Eunson <ndurrant-eunson@academic.rrc.ca>
2026-05-19 13:21:40 -0500

    Created the main and department section

---
+++ src/components/Main.tsx
@@ -0,0 +1,17 @@
+import type { Department } from "./types";
+import DepartmentSection from "./DepartmentSection";
+ 
+interface Props {
+  departments: Department[];
+}
+ 
+export default function Main({ departments }: Props) {
+  return (
+    <main>
+      {departments.map((department: Department, index: number) => (
+        <DepartmentSection key={index} department={department} />
+      ))}
+    </main>
+  );
+}
+ 
