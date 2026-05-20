c46662fe4166b0173feb7482ac7067ef05cf5630
Nicole Durrant-Eunson <ndurrant-eunson@academic.rrc.ca>
2026-05-19 19:53:43 -0500

    Wrapped the header in div, added the css in one file (app.css) and removed the contents from index.css so the original vite css doesn't interfere

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,11 @@ import logo from "../assets/logo.svg";
 export default function Header() {
     return (
         <header>
-            <img src={logo} alt="Pixell River Financial" />
-            <h1>Pixell River Employee Directory</h1>
-            <p>Welcome! Browse our talented team across all departments.</p>
-        </header>
-    );
+        <img src={logo} alt="Pixell River Financial" />
+            <div>
+            <h1>Pixell River Employee Directory </h1>
+                < p > Welcome! This is our talented team across all departments.</p>
+                    </div>
+                    </header>
+  );
 }
