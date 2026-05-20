77a3730bd204ad8057721350a0fc71bb6e4a399b
Nicole Durrant-Eunson <ndurrant-eunson@academic.rrc.ca>
2026-05-19 20:06:51 -0500

    Ran format document on header.tsx to fix the formatting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.svg";
 export default function Header() {
     return (
         <header>
-        <img src={logo} alt="Pixell River Financial" />
+        <img src= { logo } alt = "Pixell River Financial" />
             <div>
             <h1>Pixell River Employee Directory </h1>
                 < p > Welcome! This is our talented team across all departments.</p>
