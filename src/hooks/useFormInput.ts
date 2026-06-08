715055a4354a2b76f6eeeda3e3c254ece3dee911
Nicole Durrant-Eunson <ndurrant-eunson@academic.rrc.ca>
2026-06-08 16:44:22 -0500

    Created useFormInputs.ts

---
+++ src/hooks/useFormInput.ts
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export function useFormInput(initialValue: string = ""): {
+    value: string;
+    message: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
+    validate: (callback: (value: string) => string) => boolean;
+    reset: () => void;
+} {
+    const [value, setValue] = useState<string>(initialValue);
+    const [message, setMessage] = useState<string>("");
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        setValue(e.target.value);
+    };
+
+    const validate = (callback: (value: string) => string): boolean => {
+        const error = callback(value);
+        setMessage(error);
+        return error === "";
+    };
+
+    const reset = () => {
+        setValue(initialValue);
+        setMessage("");
+    };
+
+    return { value, message, onChange, validate, reset };
+}
