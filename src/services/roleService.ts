46d1b08dc844e62a075bade7af0cedb6abdf68a7
Nicole Durrant-Eunson <ndurrant-eunson@academic.rrc.ca>
2026-06-15 18:39:29 -0500

    Created the service for role

---
+++ src/services/roleService.ts
@@ -0,0 +1,14 @@
+import * as RoleRepo from "../apis/roleRepo";
+import type { Role } from "../data/types";
+
+export function getRoles(): Role[] {
+  return RoleRepo.getRoles();
+}
+
+export function createRole(firstName: string, lastName: string, role: string): Role[] {
+  if (firstName.trim().length < 3) {
+    throw new Error("First name must be at least 3 characters.");
+  }
+
+  return RoleRepo.createRole(firstName.trim(), lastName.trim(), role.trim());
+}
